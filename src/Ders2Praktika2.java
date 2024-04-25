209cb811d1f9f86534c996eddf5e8ff600f93296
elnurjalilov <92107436+elnur8453@users.noreply.github.com>
2024-04-25 12:59:54 +0400

    homework

---
+++ src/Ders2Praktika2.java
@@ -0,0 +1,10 @@
+public class Ders2Praktika2 {
+    public static void main(String[] args) {
+        int number1 = 9;
+        int number2 = 5;
+
+        int result = number1 / number2;
+
+        System.out.println("netice: " + number1 + " / " + number2 + " = " + result);
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Java");
+        System.out.println("Python");
+        System.out.println("C#");
+        System.out.println("C++");
+        System.out.println("Swift");
+    }
+}
